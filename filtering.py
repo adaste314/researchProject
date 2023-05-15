a25a746357d047d8edc38a5b3f3b13aa2e7879f8
adaste314 <adaste314@gmail.com>
2023-05-15 10:13:50 -0400

    new file, beginning of mysql implementation

--- filtering.py
+++ filtering.py
@@ -3,16 +3,32 @@ from nltk.corpus import words as wds
 import parsing
 
 nltk.download('words')
+username, passkey = parsing.username, parsing.passkey
 
-d_posts, und_posts = parsing.d_posts, parsing.und_posts
-words = []
-all_posts = d_posts + und_posts
-for i in all_posts:
-    words += i.split(" ")
+try:
+    with connect(
+        host="Serge-HP13",
+        user=username,
+        password=passkey,
+        database='data'
+    ) as connection:
 
-english_words = set(wds.words())
-common_words = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'and', 'be', 'have', 'has', 'had', 'do', 'does',
-                'did', 'will', 'would', 'shall', 'should', 'may', 'might', 'must', 'can', 'could', 'the', 'a', 'an']
-clean_words = [''.join(filter(str.isalpha, word)) for word in words]
-filtered_words = [i.lower() for i in clean_words if i.lower() in english_words and i.lower() not in common_words]
-words = {word: filtered_words.count(word) / len(filtered_words) for word in filtered_words}
+        d_posts, und_posts = parsing.d_posts, parsing.und_posts
+        words = []
+        all_posts = d_posts + und_posts
+        for i in all_posts:
+            words += i.split(" ")
+
+        english_words = set(wds.words())
+        common_words = ['am', 'is', 'are', 'was', 'were', 'being', 'been', 'and', 'be', 'have', 'has', 'had', 'do',
+                        'does',
+                        'did', 'will', 'would', 'shall', 'should', 'may', 'might', 'must', 'can', 'could', 'the', 'a',
+                        'an']
+        clean_words = [''.join(filter(str.isalpha, word)) for word in words]
+        filtered_words = [i.lower() for i in clean_words if
+                          i.lower() in english_words and i.lower() not in common_words]
+        words = {word: filtered_words.count(word) / len(filtered_words) for word in filtered_words}
+        connection.commit()
+
+except Error as e:
+    print(e)
--- parsing.py
+++ parsing.py
@@ -1,33 +1,51 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 import random
+import mysql.connector
+from mysql.connector import connect, Error
+import database_creation
 
-d_posts, und_posts = [], []
-pages_total = int(input("PAGES TO PARSE: "))
-sample_size = int(input("NUM POSTS TO PULL: "))
+username, passkey = database_creation.username, database_creation.passkey
+try:
+    with connect(
+        host="Serge-HP13",
+        user=username,
+        password=passkey,
+        database='data'
+    ) as connection:
+            d_posts, und_posts = [], []
+            pages_total = int(input("PAGES TO PARSE: "))
 
-for i in range(pages_total):
-    page = req.get("https://www.psychforums.com:443/borderline-personality/?start=" + str(i * 40)).text
-    parser = bs(page, 'html.parser')
-    d_posts += list(parser.find(id="wrap").find_all("li", class_="row bg1")) + list(
-        parser.find(id="wrap").find_all("li", class_="row bg2"))
+            for i in range(pages_total):
+                page = req.get("https://www.psychforums.com:443/borderline-personality/?start=" + str(i * 40)).text
+                parser = bs(page, 'html.parser')
+                d_posts += list(parser.find(id="wrap").find_all("li", class_="row bg1")) + list(
+                    parser.find(id="wrap").find_all("li", class_="row bg2"))
 
-    page = req.get("https://www.psychforums.com:443/antisocial-personality/?start=" + str(i * 40)).text
-    parser = bs(page, 'html.parser')
-    d_posts += list(parser.find(id="wrap").find_all("li", class_="row bg1")) + list(
-        parser.find(id="wrap").find_all("li", class_="row bg2"))
+                page = req.get("https://www.psychforums.com:443/antisocial-personality/?start=" + str(i * 40)).text
+                parser = bs(page, 'html.parser')
+                d_posts += list(parser.find(id="wrap").find_all("li", class_="row bg1")) + list(
+                    parser.find(id="wrap").find_all("li", class_="row bg2"))
 
-    page = req.get("https://www.gardening-forums.com/forums/general-gardening-talk.5/page-" + str(i)).text
-    parser = bs(page, 'html.parser')
-    und_posts += list(parser.find(id="top").find_all("div", class_="structItem-title"))
+                page = req.get("https://www.gardening-forums.com/forums/general-gardening-talk.5/page-" + str(i)).text
+                parser = bs(page, 'html.parser')
+                und_posts += list(parser.find(id="top").find_all("div", class_="structItem-title"))
 
-d_posts = random.sample([str(i).split('href="')[1].split('"')[0] for i in d_posts], sample_size)
-und_posts = random.sample([str(i).split('href="')[1].split('"')[0] for i in und_posts], sample_size)
+            d_posts = [str(i).split('href="')[1].split('"')[0] for i in d_posts]
+            und_posts = ["https://www.gardening-forums.com/" + str(i).split('href="')[1].split('"')[0] for i in und_posts]
+            insert_values = [(i, '') for i in d_posts + und_posts]
+            cursor.executemany("INSERT INTO posts(link, words) VALUES (%s, %s)", insert_values)
 
-for i in d_posts:
-    parser = bs(req.get(i).text, 'html.parser')
-    d_posts[d_posts.index(i)] = str(parser.find('div', class_="content")).split('">')[1].split('</')[0]
+            for i in d_posts:
+                parser = bs(req.get(i).text, 'html.parser')
+                d_posts[d_posts.index(i)] = str(parser.find('div', class_="content")).split('">')[1].split('</')[0]
 
-for i in und_posts:
-    parser = bs(req.get("https://www.gardening-forums.com/" + str(i)).text, 'html.parser')
-    und_posts[und_posts.index(i)] = " ".join(str(parser.find('div', class_="bbWrapper")).split('"'))
+            for i in und_posts:
+                parser = bs(req.get(i).text, 'html.parser')
+                und_posts[und_posts.index(i)] = " ".join(str(parser.find('div', class_="bbWrapper")).split('"'))
+
+            connection.commit()
+
+except Error as e:
+    print(e)
+
